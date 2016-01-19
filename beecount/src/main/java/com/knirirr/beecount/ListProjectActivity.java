088419e466360026275a4e4cbfd6c82c399e1bf7
Milo Thurston <knirirr@gmail.com>
2016-01-19 22:38:57 +0000

    Activity change to avoid annoying crash.

--- beecount/src/main/java/com/knirirr/beecount/ListProjectActivity.java
+++ beecount/src/main/java/com/knirirr/beecount/ListProjectActivity.java
@@ -4,6 +4,7 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
